4864e98c9907708465f11a6e145254c12ca50be6
Mahdi <mahdi.gerami77@gmail.com>
2024-10-06 23:49:16 +0330

    fix hook mesh and create ball

--- character.gd
+++ character.gd
@@ -2,18 +2,38 @@ extends CharacterBody3D
 
 var motion = Vector3()
 
-const accel = 100
+const accel = 50
 const gravity = 30
 
-var ground_max_speed = 200
-var air_max_speed = 500
+var ground_max_speed = 100
+var air_max_speed = 100
 
 var swing_point = null
 var swing_radius = null
 
-@onready var camera_3d: Camera3D = $Camera3D
-@onready var hook_ray_cast: RayCast3D = $Camera3D/HookRayCast
-@onready var hook_line: MeshInstance3D = $Camera3D/HookLine
+@onready var head: Node3D = $Head
+@onready var camera_spring_arm: SpringArm3D = $Head/CameraSpringArm
+@onready var hook_line: Node3D = $HookLine
+@onready var hook_ray_cast: RayCast3D = $Head/CameraSpringArm/HookRayCast
+
+@onready var target_rotation = rotation
+const mouse_sens = 0.2
+const max_turn_speed = 0.3
+
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	pass
+	
+
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		rotate_y(deg_to_rad(-event.relative.x * mouse_sens))
+		# We need to rotate the head instead of the entire character.
+		camera_spring_arm.rotate_x(deg_to_rad(-event.relative.y * mouse_sens))
+		# clamp locks min/max of a value. Here we use it to make sure we can't look up/down more than 90 degrees
+		camera_spring_arm.rotation.x = clamp(camera_spring_arm.rotation.x, deg_to_rad(-90), deg_to_rad(80))
 
 # decelerate movement for when no move key is pressed
 func decel(value, delta):
@@ -26,7 +46,7 @@ func _physics_process(delta: float) -> void:
 	
 	# We handle x and z movement/motion separately in a vector 2d and later on add y and convert to vector 3d
 	# x and z are rotated with camera so forward is always in the direction of camera
-	var motion_2d = Vector2(motion.x, motion.z).rotated(camera_3d.rotation.y)
+	var motion_2d = Vector2(motion.x, motion.z).rotated(rotation.y)
 	
 	var movement_normal = Vector2(
 		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
@@ -36,7 +56,7 @@ func _physics_process(delta: float) -> void:
 	
 	if is_on_floor():
 		if Input.is_action_just_pressed("jump"):
-			motion.y = 20
+			motion.y = 80
 		
 		if movement_normal.x == 0:
 			motion_2d.x = decel(motion_2d.x, delta)
@@ -61,7 +81,7 @@ func _physics_process(delta: float) -> void:
 	
 	# We rotated the motion based on camera earlier so move inputs are according to camera's facing direction.
 	# Now we need to revert the rotate so movement is according to world space.
-	motion_2d = motion_2d.rotated(-camera_3d.rotation.y)
+	motion_2d = motion_2d.rotated(-rotation.y)
 	# Here we add a Y access to our motion and make it 3d
 	motion = Vector3(motion_2d.x, motion.y, motion_2d.y)
 	set_velocity(motion)
@@ -70,9 +90,9 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 	motion = velocity
 	
+	# Hook section
 	handle_hook()
 	
-	# Hook section
 	
 	
 func handle_hook():
@@ -86,7 +106,7 @@ func handle_hook():
 	
 	if swing_point == null:
 		hook_line.hide()
-		camera_3d.target_rotation.z = 0
+		#camera_3d.target_rotation.z = 0
 	else:
 		var point_difference = global_transform.origin - swing_point
 		var point_distance = point_difference.length()
@@ -104,8 +124,8 @@ func handle_hook():
 		hook_line.look_at(swing_point, Vector3(0, 1, 0))
 		hook_line.show()
 		
-		var tilt_scale = point_normal.dot(Vector3(1, 0, 0).rotated(Vector3(1, 0, 0), camera_3d.rotation.x).rotated(Vector3(0, 1, 0), $Camera3D.rotation.y))
-		camera_3d.target_rotation.z = tilt_scale * PI / 8
+		#var tilt_scale = point_normal.dot(Vector3(1, 0, 0).rotated(Vector3(1, 0, 0), camera_3d.rotation.x).rotated(Vector3(0, 1, 0), $Camera3D.rotation.y))
+		#camera_3d.target_rotation.z = tilt_scale * PI / 8
 	
 	##hook range indicator
 	#if hook_ray_cast.is_colliding():
